1aba52b73404799127cb075f7a0e0f5d7910aa7d
Justen Caldwell <32503421+JustenC14@users.noreply.github.com>
2018-04-08 09:36:41 -0400

    Output functionality  + cleaned up
    1. Added an output file functionality that shows all calculations as they occur
    2. Removed 2 variables that weren't even being used.
    3. Cleaned up by adding some new functions so the main looks better

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from web_scraper import clean_string
 from web_scraper import web_scraper
 from collections import Counter
 from tabulate import tabulate
-from copy import deepcopy
 
 def get_collection(doc_collection):
     while True:  #Loop that aquires web-pages and builds doc_collection
@@ -14,13 +13,12 @@ def get_collection(doc_collection):
        try:
            title, body = web_scraper(web_page)
            doc_collection.append([title, body])
-           doc_dict.append(set(body))
        except:
            print('Error: Invalid Webpage Entry')
     return(doc_collection)
 
-def term_frequency(list_in):
-    term_count = Counter(list_in)
+def term_frequency(term_list):
+    term_count = Counter(term_list)
     count_list = []
     for word, count in term_count.items():
         count_log = (math.log1p(count))
@@ -41,70 +39,103 @@ def document_frequency(query, doc_collection):
         query[i].append(1 + math.log((1 + len(doc_collection)) / (1 + doc_freq))) #Add the document frequency to the list
     return(query)
 
-def query_normalize(query_in):
-        #Calculate the length of the query vector for normalization
-        length = 0
-        for i in range(len(query_in)):
-            length += (query_in[i][5]**2)
-        length = math.sqrt(length)
-        #Normalize all values in the query vector.
-        for i in range(len(query_in)):
-            query_in[i].append(query_in[i][5] / length)
+#Just multiplies the IDF * TF_WT
+def calculate_idfWT(query):
+    for i in range (len(query)):
+        query[i].append(query[i][2] * query[i][4])
+    return(query)
 
-        return(query_in)
+def query_normalize(query):
+    #Calculate the length of the query vector for normalization
+    length = 0
+    for i in range(len(query)):
+        length += (query[i][5]**2)
+    length = math.sqrt(length)
+    #Normalize all values in the query vector.
+    for i in range(len(query)):
+        query[i].append(query[i][5] / length)
 
-def document_normalize(body):
+    return(query)
+
+def document_normalize(document_body):
     #Calculate the length of the body vector for normalization
     length = 0
-    for i in range(len(body)):
-        length += (body[i][2]**2)
+    for i in range(len(document_body)):
+        length += (document_body[i][2]**2)
     length = math.sqrt(length)
     #Normalize all values in the body vector.
-    for i in range(len(body)):
-        body[i].append(body[i][2] / length)
+    for i in range(len(document_body)):
+        document_body[i].append(document_body[i][2] / length)
 
-    return(body)
+    return(document_body)
 
+#Calculates the cosine similarity between the query and body provided
 def calculate_similarity(query, document_body):
     similarity_score = 0
     for i in range(len(query)):
         word = query[i][0]
-        for term, tf, tf_wt, normalize in body:
+        #Find all terms that are in both the document body and query
+        #These are the only terms that matter for calculations, because
+        #If the term is in the query and not in the body then it will result
+        #in a 0 when multiplied.
+        for term, tf, tf_wt, normalize in document_body:
             if word == term:
+                #Multiply the IDF_WT-Normalized to the TF_WT normalized
+                #Add this to the total similarity score to be returned
+                output_file.write("Found: {0}.\n".format(term))
+                output_file.write("Similarity calculation: {0}: {1} * {2} = {3}\n".format(term, query[i][6], normalize, query[i][6] * normalize))
                 similarity_score += (query[i][6] * normalize)
+                output_file.write("Current similarity score: {0}\n\n".format(similarity_score))
 
     return(similarity_score)
 
+def file_output(query, doc_collection):
+    #Create the output file
+    #Put the query table at the top for viewing
+    output_file.write('\n\nQUERY TABLE\n')
+    output_file.write(tabulate(query, headers=['term', 'tf_raw', 'tf_wt', 'df', 'idf', 'wt', 'n\'lize']))
+    output_file.write('\n\nDOCUMENT TABLES\n')
+    for title, body in doc_collection:
+        output_file.write(title)
+        output_file.write('\n')
+        body.sort(key = lambda x: x[1], reverse = True)
+        output_file.write(tabulate(body, headers=['term', 'tf_raw', 'tf_wt', 'n\'lize']))
+        output_file.write('\n')
+
 if __name__ == "__main__":
-    doc_collection = [] #This list will store the title and dictionary for our documents
-    doc_dict = [] #Because of the way I am appending info as the program executes
-                  #It is important to store just the raw terms for calc df
+    output_file = open("info_output.txt", "w+")
+
+    doc_collection = []
     doc_collection = get_collection(doc_collection)
+    query = term_frequency(clean_string(input('Enter Query: ')))
 
     #Calculate all of the term frequencies for the document_collection.
     for i in range(len(doc_collection)):
-        temp = term_frequency(doc_collection[i][1])
-        doc_collection[i][1] = temp
+        doc_collection[i][1] = term_frequency(doc_collection[i][1])
 
-    #Gets the query and cleans it. Counts the TF of the query.
-    query = term_frequency(clean_string(input('Enter Query: ')))
-    query_list = document_frequency(query, doc_collection)
-
+    #calculate doc frequency for each word in the query
+    query = document_frequency(query, doc_collection)
     #Calculate the IDF * TF_WT value.
-    for i in range (len(query_list)):
-        query_list[i].append(query_list[i][3] * query_list[i][4])
-
+    query = calculate_idfWT(query)
     #Normalize the IDF * TF_WT values for use in calculating cosine similarity score
-    query_list = query_normalize(query_list)
+    query = query_normalize(query)
 
+    #Normalize the TF_WT for the document bodies
     for title, body in doc_collection:
         body = document_normalize(body)
 
+    #Create a list that will store the title and similarity score for every doc
     doc_similarity = []
+    output_file.write('CALCULATING SIMILARITY SCORES FOR DOCUMENTS\n')
     for title, body in doc_collection:
+        output_file.write('\nDOCUMENT: {0}\n'.format(title))
         doc_similarity.append([title, calculate_similarity(query, body)])
 
-    print(tabulate(query, headers=['term','tf','tf_wt','df','idf','idf_wt','nmlize']))
+    #Sort the list to provide the most similar documents on top
     doc_similarity.sort(key = lambda x: x[1], reverse = True)
 
-    print('\n\n', tabulate(doc_similarity, headers=['Title', 'Score']))
+    file_output(query, doc_collection)
+    #Print the similarity scores most relevant to least with tabulate function
+    #for increased readability
+    print(tabulate(doc_similarity, headers=['Title', 'Score'], showindex="always", tablefmt="fancy_grid"), '\n')
+    output_file.close()
